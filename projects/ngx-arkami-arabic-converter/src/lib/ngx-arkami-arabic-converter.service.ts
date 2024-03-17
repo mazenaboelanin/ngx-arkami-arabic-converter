98d5b9177c9c2a293b732f4814baa27047b44cfb
Mazen Abo Elanin Ahmed <mazenaboelanin@gmail.com>
2024-03-17 23:06:09 +0200

    feat: create a generic method to convert numbers to arabic words

--- projects/ngx-arkami-arabic-converter/src/lib/ngx-arkami-arabic-converter.service.ts
+++ projects/ngx-arkami-arabic-converter/src/lib/ngx-arkami-arabic-converter.service.ts
@@ -11,5 +11,36 @@ export class NgxArkamiArabicConverterService {
   private millions = ['', 'مليون', 'مليونان', 'ثلاثة ملايين', 'أربعة ملايين', 'خمسة ملايين', 'ستة ملايين', 'سبعة ملايين', 'ثمانية ملايين', 'تسعة ملايين'];
   private teens = ['', 'أحد عشر', 'اثنا عشر', 'ثلاثة عشر', 'أربعة عشر', 'خمسة عشر', 'ستة عشر', 'سبعة عشر', 'ثمانية عشر', 'تسعة عشر'];
 
-  constructor() { }
+constructor() { }
+
+convertToArabicWords(strN: string): string {
+  let result = '';
+  let length = strN.length;
+  switch (length) {
+    case 7:
+      result = this.sevenDigitsToArabic(strN);
+      break;
+    case 6:
+      result = this.sixDigitsToArabic(strN);
+      break;
+    case 5:
+      result = this.fiveDigitsToArabic(strN);
+      break;
+    case 4:
+      result = this.FourDigitsToArabic(strN);
+      break;
+    case 3:
+      result = this.threeDigitsToArabic(strN);
+      break;
+    case 2:
+      result = this.twoDigitToArabic(strN);
+      break;
+    case 1:
+      result = this.oneDigitToArabic(strN);
+      break;
+  }
+
+  return 'فقط ' + result.trim() + (parseInt(strN) === 1 ? ' جنيه مصري لا غير' : ' جنيهات مصرية لا غير');
+}
+
 }
